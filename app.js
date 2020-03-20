809fa69a78af62a56082a0768b9ec4d83d692b38
Emil Öhman <emil.ohman@svenskaspel.se>
2020-03-20 21:21:07 +0100

    Fixed hot water boost and ventilation boost

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ class NibeApp extends OAuth2App {
 
   onOAuth2Init() {
 		//this.enableOAuth2Debug();
+
 		this.setOAuth2Config({
 			apiUrl: NibeOAuth2Client.API_URL,
 			client: NibeOAuth2Client,
 			tokenUrl: `${NibeOAuth2Client.API_URL}/oauth/token`,
 			authorizationUrl: `${NibeOAuth2Client.API_URL}/oauth/authorize`,
 			scopes: ['READSYSTEM', 'WRITESYSTEM'],
+			allowMultiSession: true
 		});
 
 	}
